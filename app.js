4f51dd9ad75e34e41bdcea3043d20a372b186e11
Mustafa Paksoy <paksoy@gmail.com>
2019-05-26 21:47:34 -0700

    wip

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-var express = require('express');
-var path = require('path');
+var express = require("express");
+var path = require("path");
 
 var app = express();
 
-var hbs = require('express-handlebars')({
-  defaultLayout: 'main',
-  extname: '.hbs'
+var hbs = require("express-handlebars")({
+  defaultLayout: "main",
+  extname: ".hbs"
 });
-app.engine('hbs', hbs);
-app.set('view engine', 'hbs');
+app.engine("hbs", hbs);
+app.set("view engine", "hbs");
 
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  express.static(path.join(__dirname, "node_modules", "bootstrap", "dist"))
+);
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', function(req, res) {
-  res.render('index');
+app.get("/", function(req, res) {
+  res.render("index");
 });
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log('Express started, listening to port: ', port);
+  console.log("Express started, listening to port: ", port);
 });
